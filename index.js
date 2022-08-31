e68575dd549df20a53b8d3499e8a89ba03931ee0
triwibow <triw3707@gmail.com>
2022-08-31 16:46:18 +0700

    add fix bug

--- index.js
+++ index.js
@@ -8,23 +8,22 @@ const routerV1 = require('./src/routes/routeV1');
 app.use(express.json());
 app.use(express.static('uploads'));
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*")
-//     res.header(
-//       "Access-Control-Allow-Headers",
-//       "*"
-//     )
-//     if (req.method === "OPTIONS") {
-//       res.header(
-//         "Access-Control-Allow-Methods",
-//         "POST, PUT, PATCH, GET, DELETE"
-//       )
-//       return res.status(200).json({})
-//     }
-//     next()
-// });
-
-app.use(cors({ origin: "*", credentials: true,  optionsSuccessStatus: 200 }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "*"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+    //   return res.status(200).json({})
+    }
+    // next()
+    return res.status(200).json({})
+});
 
 app.use('/api/v1/', routerV1);
 
