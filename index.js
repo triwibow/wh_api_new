74217276d2ae5087e2aa88beedeb8fee7d82245f
triwibow <triw3707@gmail.com>
2022-08-31 16:23:08 +0700

    add fix bug

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(express.static('uploads'));
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
+    // res.header(
+    //   "Access-Control-Allow-Headers",
+    //   "Origin, X-Requested, Content-Type, Accept Authorization"
+    // )
     if (req.method === "OPTIONS") {
       res.header(
         "Access-Control-Allow-Methods",
