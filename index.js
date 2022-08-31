8c7a856c91d518406cc08c3edc47628570373478
triwibow <triw3707@gmail.com>
2022-08-31 16:18:40 +0700

    add fix bug

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const routerV1 = require('./src/routes/routeV1');
 
 app.use(express.json());
 app.use(express.static('uploads'));
-app.use(cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+});
+
 app.use('/api/v1/', routerV1);
-app.options('*', cors());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
