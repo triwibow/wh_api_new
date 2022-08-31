1a3078ea4defdaa47e264670466ba861195bbdef
triwibow <triw3707@gmail.com>
2022-08-31 14:11:15 +0700

    add fix bug

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 require('dotenv').config();
-var zlib = require('zlib');
-var path = require('path');
-const fs = require('fs'); 
-var tar = require('tar-stream');
-
 const express = require('express');
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-var exec = require('child_process').exec;
-
 const routerV1 = require('./src/routes/routeV1');
-const e = require('express');
-
-
 
 app.use(express.json());
 app.use(express.static('uploads'));
 app.use(cors())
 
 app.use('/api/v1/', routerV1);
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
