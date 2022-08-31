54d4e4a853fb4012629f63f58d7f7d6179be5a9e
triwibow <triw3707@gmail.com>
2022-08-31 15:56:22 +0700

    add fix bug

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const routerV1 = require('./src/routes/routeV1');
 app.use(express.json());
 app.use(express.static('uploads'));
 app.use(cors());
-
 app.use('/api/v1/', routerV1);
+app.options('*', cors());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
