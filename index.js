e19a190b67161a68391bbec8827243d17b905344
triwibow <triw3707@gmail.com>
2022-08-31 16:51:38 +0700

    add fix bug

--- index.js
+++ index.js
@@ -7,24 +7,7 @@ const routerV1 = require('./src/routes/routeV1');
 
 app.use(express.json());
 app.use(express.static('uploads'));
-
-app.use((req, res) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "*"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      
-    }
-
-    return res.status(200).json({})
-});
-
+app.use(cors());
 app.use('/api/v1/', routerV1);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
