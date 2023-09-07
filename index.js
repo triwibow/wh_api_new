a06b2917dbba3466df0a47443b24ed00b9a8283d
Tri Wibowo <triw@oninyon.com>
2023-09-07 15:08:39 +0700

    - asd

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require('cors')
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('uploads'));
+
 const routerV1 = require('./src/routes/routeV1');
 
-app.use(express.json());
-app.use(express.static('uploads'));
-app.use(cors());
 app.use('/api/v1/', routerV1);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
