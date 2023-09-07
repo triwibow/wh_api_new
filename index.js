2da6737cee02a017981c8cf161260dabba181c10
Tri Wibowo <triw@oninyon.com>
2023-09-07 15:38:58 +0700

    -aksd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
